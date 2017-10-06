f406e8daeb633d819cbfea7a300b581205464832
Mark Engen <markengen@marks-air>
2017-10-06 00:59:16 -0400

    Added append logic to clear initial 0 from display when adding numbers via the buttons.  Also added some comments.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -13,9 +13,19 @@ class ViewController: UIViewController {
     //"display" is the variable name we gave to the connection
     @IBOutlet var display: UILabel!  //Can hover over the circle to the left to highlight the object in the view.  Also note, @IBOutlet is something Xcode creates (i.e. we don't type it to create a connection).
     //all instances of classes live in the heap and memory is managed for you via reference counting.
+    //In this case, the var is initialized and assigned a value.  Had the value not been assigned, it would've needed an initializer to give it a literal value or a parameter argument reference value.
+    
+    var userIsInTheMiddleOfTypingANumber: Bool = false
     
     @IBAction func appendDigit(_ sender: UIButton) {
         let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
         print("digit = \(String(describing: digit))")
     }
 }
